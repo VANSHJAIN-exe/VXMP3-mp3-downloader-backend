b00976ce8c3fa4e0e9900a99d70e7f6693cd030d
Vansh Jain <165574262+VANSHJAIN-exe@users.noreply.github.com>
2025-03-23 03:09:49 +0530

    backend communication changes

--- server.js
+++ server.js
@@ -47,3 +47,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "https://vxmp3.vercel.app", 
+    credentials: true,
+  })
+);
