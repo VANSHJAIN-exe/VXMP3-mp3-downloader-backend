618635130839c326bb176904ca8e975dc22fd258
Vansh Jain <165574262+VANSHJAIN-exe@users.noreply.github.com>
2025-03-23 02:42:01 +0530

    Made the port dynamical in the server

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post('/download', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
